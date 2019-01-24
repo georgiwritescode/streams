11889d7712bf3ade5c9a3c2a4dd253d258ba4e2b
Georgi Ivanov <g.ivanov@blubito.com>
2019-01-24 13:53:43 +0200

    scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './steams/StreamCreate';
+import StreamEdit from './steams/StreamEdit';
+import StreamDelete from './steams/StreamDelete';
+import StreamShow from './steams/StreamShow';
+import StreamList from './steams/StreamList';
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
