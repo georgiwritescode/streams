797351690310fb697399ddaf9c2ea599b6365eb8
Georgi Ivanov <g.ivanov@blubito.com>
2019-02-13 16:00:32 +0200

    delete a stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -34,7 +34,8 @@ export const fetchStream = (id) => async dispatch => {
 
 export const deleteStream = (id) => async dispatch => {
     const response = await streams.delete(`/streams/${id}`);
-    dispatch({ type: DELETE_STREAM, payload: response.data });
+    dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 }
 
 export const editStream = (id, formValues) => async dispatch => {
